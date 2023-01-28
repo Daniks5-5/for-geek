844b883269b66195d1cee23285000450fe381270
unknown <ssanym211@gmail.com>
2023-01-28 20:47:09 +0300

    first commit

---
+++ work1/квадрат числа.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число :");
+double m = double.Parse(Console.ReadLine()!); 
+double f = m*m;
+Console.WriteLine("квадрат числа :");
+Console.WriteLine(f);
---
+++ По двум заданным числам проверять является ли первое квадратом второго/work2.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите два числа : ");
+double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine(a);
+double c = a*a;
+if(c==b){
+    Console.WriteLine("первое число является квадратом второго числа");
+}
+else {
+    Console.WriteLine("первое число не является квадротома второго");
+}
