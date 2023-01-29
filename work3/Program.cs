436fc264192813fb635b2a0ce88c55fb8ca1005e
unknown <ssanym211@gmail.com>
2023-01-29 17:21:30 +0300

    new commits

---
+++ work3/Program.cs
@@ -0,0 +1,5 @@
+﻿string[] stringarr;
+stringarr=new string[8]{"такого дня нет","понедельник","вторник","среда","четверг","пятница","суббота","воскресенье"};
+Console.WriteLine("Введите номер дня :");
+int a= int.Parse(Console.ReadLine()!);
+
---
+++ work4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите числа :");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int max=a;
+if(a<b) max=b;
+if(b<c) max=c;
+Console.WriteLine("Самое большое число:");
+Console.Write(max);
